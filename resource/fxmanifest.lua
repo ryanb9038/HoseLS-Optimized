625b70fb9a06503a534469799f0905fd3b768735
Harv <86970698+Hxrv3y@users.noreply.github.com>
2024-10-23 00:26:49 +0100

    Tweak(Manifest): Remove annoying prints in console

--- resource/fxmanifest.lua
+++ resource/fxmanifest.lua
@@ -14,10 +14,7 @@ server_scripts {
 }
 
 files {
-    'hose/contentunlocks.meta',
-	'hose/loadouts.meta',
 	'hose/pedpersonality.meta',
-	'hose/shop_weapon.meta',
 	'hose/weaponanimations.meta',
 	'hose/weaponarchetypes.meta',
 	'hose/weapons.meta',
@@ -25,10 +22,7 @@ files {
 
 data_file 'WEAPONINFO_FILE' 'hose/weapons.meta'
 data_file 'WEAPON_METADATA_FILE' 'hose/weaponarchetypes.meta'
-data_file 'WEAPON_SHOP_INFO' 'hose/shop_weapon.meta'
 data_file 'WEAPON_ANIMATIONS_FILE' 'hose/weaponanimations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'hose/contentunlocks.meta'
-data_file 'LOADOUTS_FILE' 'hose/loadouts.meta'
 data_file 'PED_PERSONALITY_FILE' 'hose/pedpersonality.meta'
 
 -- Join the London Studios Discord here: https://discord.gg/F2zmUTD
